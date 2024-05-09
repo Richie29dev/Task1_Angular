3473d43212489bde7c4d898fdfced51baad44207
Richie29dev <rahul@rasheedfoundation.org>
2024-05-09 13:59:29 +0530

    first upload

---
+++ my-angular-app/src/app/HelloComponent/hello.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+@Component({
+selector: 'app-hello',
+templateUrl: './hello.component.html',
+styleUrls: ['./hello.component.css']
+})
+export class HelloComponent { 
+    title = 'my-angular-app'
+}
